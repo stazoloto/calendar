9859d27a9502d554d8d5fe031c87872efeed9abf
Stanislav <stazoloto@gmail.com>
2025-05-04 22:58:54 +0300

    create server

---
+++ calendar_server/lib/server.dart
@@ -0,0 +1,26 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_auth_server/serverpod_auth_server.dart' as auth;
+
+import 'src/generated/protocol.dart';
+import 'src/generated/endpoints.dart';
+
+// This is the starting point of your Serverpod server. In most cases, you will
+// only need to make additions to this file if you add future calls,  are
+// configuring Relic (Serverpod's web-server), or need custom setup work.
+
+void run(List<String> args) async {
+  // Initialize Serverpod and connect it with your generated code.
+  final pod = Serverpod(
+    args,
+    Protocol(),
+    Endpoints(),
+    authenticationHandler: auth.authenticationHandler,
+  );
+
+  auth.AuthConfig.set(auth.AuthConfig(
+    minPasswordLength: 8,
+  ));
+
+  await pod.start();
+
+}
